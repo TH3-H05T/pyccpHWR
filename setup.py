93e683fe166d3562ce55c6a6bd40340e36fef2aa
TH3-H05T <arya.mortazavi2004@gmail.com>
2023-03-15 15:22:16 +0000

    get python working

--- setup.py
+++ setup.py
@@ -1,33 +1,19 @@
-#!/bin/env python
-
-import os
-from setuptools import setup, find_packages
-from glob import glob
-
-def packagez(base):
-    return  ["{0!s}{1!s}{2!s}".format(base, os.path.sep, p) for p in find_packages(base)]
+from setuptools import setup
 
 setup(
-    name = 'pyccp',
-    version = '0.9.0',
-    provides = ['pyccp'],
-    description = "CAN Calibration Protocol for Python",
-    author = 'Christoph Schueler',
-    author_email = 'cpu12.gems@googlemail.com',
-    url = 'http://github.com/pySART/pyccp',
-    packages = packagez('pyccp'),
-    install_requires = ['enum34', 'future', 'mako'],    # 'mock'
-    #entry_points = {
-    #    'console_scripts': [
-    #            'readelf.py = objutils.tools.readelf:main',
-    #    ],
-    #},
-    #data_files = [
-    #    ('objutils/tests/ELFFiles', glob('objutils/tests/ELFFiles*.*')),
-    #],
-    package_dir = {'tests': 'pyccp/tests'},
-    #package_data = {'tests': ['ELFFiles/*.*']},
-    #include_package_data = True,
-    test_suite = "pyccp.tests"
+    name='pyccpHWR',
+    version='0.1',
+    packages=['pyccp', 'pyccp.tests'],
+    url='https://github.com/TH3-H05T/pyccpHWR',
+    license='',
+    author='aryam',
+    author_email='arya.mortazavi2004@gmail.com',
+    description='ccp library for HWR'
 )
+install_requires = [
+    'enum34',
+    'mock',
+    'future',
+    'mako',
 
+]
